b38f389972ad44d25dcf12df0619de65eb40324c
Patrick <tbiclientesltda@gmail.com>
2026-08-07 11:18:25 -0300

    fix(build): increase workbox maximumFileSizeToCacheInBytes to 5MB to prevent Vercel CI crash

--- vite.config.ts
+++ vite.config.ts
@@ -36,6 +36,7 @@ export default defineConfig(() => {
           ]
         },
         workbox: {
+          maximumFileSizeToCacheInBytes: 5000000,
           globPatterns: ['**/*.{js,css,html,ico,png,svg,json}'],
           runtimeCaching: [
             {
